5c10ea72ddcf3d097942b42be5bf3a838ea82f4b
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-02-27 01:10:27 +0530

    Update recursiveLinSearch.py

--- recursiveLinSearch.py
+++ recursiveLinSearch.py
@@ -1,15 +1,15 @@
-def linSearchRecursive(arr, ele, i):
+def recursiveLinearSearch(arr, ele, i):
     if i == len(arr):
         print("Element not found")
         return
 
     if arr[i] == ele:
-        print("Element found at", i)
+        print("Element found at " + i)
         return
     else:
-        linSearchRecursive(arr, ele, i+1)
+        recursiveLinearSearch(arr, ele, i+1)
 
 arr = list(map(int, input().split()))
 ele = int(input())
 
-linSearchRecursive(arr, ele, 0)
+recursiveLinearSearch(arr, ele, 0)
