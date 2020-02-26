c587b7750e3e0266172b40b8144e867bbe712c32
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-02-27 00:23:12 +0530

    Create stringMatch.py

---
+++ stringMatch.py
@@ -0,0 +1,16 @@
+def string_match(text, pattern):
+    n, m = len(text), len(pattern)
+    for i in range(0, n-m+1):
+        j = 0
+        while j < m and text[i+j] == pattern[j]:
+            j += 1
+        if j == m:
+            return i
+    return -1
+
+print(string_match("Cool string matching algo", "string"))
+print(string_match("computer science engineering", "science"))
+print(string_match("working is fun", "work"))
+print(string_match("cabaaabc", "abc"))
+print(string_match("bccdabc", "bc"))
+print(string_match("aaaaabc", "bcd"))
