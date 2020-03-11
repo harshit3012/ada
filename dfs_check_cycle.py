3014943b75a7353048b614e15c29b90890d97d02
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-03-12 02:16:33 +0530

    Update dfs_check_cycle.py

--- dfs_check_cycle.py
+++ dfs_check_cycle.py
@@ -1,3 +1,4 @@
+"""
 import string
 n = int(input("Enter the number of nodes : "))
 out_graphs1 = {}
@@ -7,3 +8,52 @@ for i in list(string.ascii_lowercase)[0:n]:
 print(out_graphs1.keys())
 visited = {}
 for i in list(out_graphs1.keys()):
+"""
+from collections import defaultdict 
+  
+class fin_graph(): 
+    def __init__(self,vertices): 
+        self.graph = defaultdict(list) 
+        self.V = vertices 
+  
+    def addEdge(self,u,v): 
+        self.graph[u].append(v) 
+  
+    def isCyclicUtil(self, v, visited, recStack): 
+        visited[v] = True
+        recStack[v] = True
+        for neighbour in self.graph[v]: 
+            if !visited[neighbour]: 
+                if self.isCyclicUtil(neighbour, visited, recStack): 
+                    return True
+            elif recStack[neighbour]: 
+                return True
+        recStack[v] = False
+        return False
+
+    def isCyclic(self): 
+        visited = [False] * self.V 
+        recStack = [False] * self.V 
+        for node in range(self.V): 
+            if ivisited[node]: 
+                if self.isCyclicUtil(node,visited,recStack) == True: 
+                    return True
+        return False
+  
+g = fin_graph(6) 
+g.addEdge(0, 4) 
+g.addEdge(0, 2) 
+g.addEdge(1, 3) 
+g.addEdge(1, 5) 
+g.addEdge(2, 3) 
+g.addEdge(3, 6)
+g.addEdge(4, 3)
+g.addEdge(4, 6)
+g.addEdge(5, 4)
+g.addEdge(6, 0)
+
+if g.isCyclic(): 
+    print("Graph has a cycle")
+else: 
+    print("Graph has no cycle")
+  
