8889768891b5d6fb650d9e55c7624f22720ae5b1
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-03-11 23:16:57 +0530

    Add files via upload

---
+++ bin_search_rec.py
@@ -0,0 +1,45 @@
+import random as rm
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from sklearn import linear_model as lr
+#from numpy import polyfit
+
+
+def recBinSearch(arr, ele, high, low):
+    if low > high:
+        #print("Element not found")
+        return
+
+    mid = (high + low) // 2
+    if arr[mid] == ele:
+        #print("Element found at", mid)
+        return
+    elif arr[mid] > ele:
+        high = mid - 1
+    else:
+        low = mid + 1
+
+    recBinSearch(arr, ele, high, low)
+
+
+ranges = 10
+yplot = []
+xplot = []
+while(ranges <= 1000):
+    arr = [rm.randint(0, ranges) for _ in range(0, ranges, 1)]
+    ele = arr[len(arr)-1]
+    st1 = time.process_time()
+    recBinSearch(arr, ele, len(arr)-1, 0)
+    tt = time.process_time() - st1
+    yplot.append(tt)
+    xplot.append(ranges)
+    ranges = ranges + 10
+
+#xp1 = np.array(xplot)
+#yp1 = np.array(yplot)
+#reg1 = lr.LinearRegression()
+#reg1.fit(xp1.reshape(-1, 1), yplot)
+#plt.plot(xp1, reg1.predict(xp1.reshape(-1, 1)), color="blue")
+plt.scatter(xplot, yplot, color="red", marker="+")
+plt.show()
---
+++ linear_search_rec_visual.py
@@ -0,0 +1,39 @@
+import random as rm
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from sklearn import linear_model as lr
+
+
+def recursiveLinearSearch(arr, ele, i):
+    if i == len(arr):
+        # print("Element not found")
+        return
+
+    if arr[i] == ele:
+        # print("Element found at " + str(i))
+        return
+    else:
+        return recursiveLinearSearch(arr, ele, i+1)
+
+
+ranges = 10
+yplot = []
+xplot = []
+while(ranges <= 1000):
+    arr = [rm.randint(0, ranges) for _ in range(0, ranges, 1)]
+    ele = arr[len(arr)-1]
+    st1 = time.process_time()
+    recursiveLinearSearch(arr, ele, 0)
+    tt = time.process_time() - st1
+    yplot.append(tt)
+    xplot.append(ranges)
+    ranges = ranges + 50
+
+xp1 = np.array(xplot)
+yp1 = np.array(yplot)
+reg1 = lr.LinearRegression()
+reg1.fit(xp1.reshape(-1, 1), yplot)
+plt.plot(xp1, reg1.predict(xp1.reshape(-1, 1)), color="blue")
+plt.scatter(xp1, yp1, color="red", marker="+")
+plt.show()
