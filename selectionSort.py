9c9b938674230def78df6b07df5c5a08b0f62925
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-03-12 02:03:27 +0530

    Update selectionSort.py

--- selectionSort.py
+++ selectionSort.py
@@ -1,21 +1,26 @@
 import random
 import time as tm
 import os
+import matplotlib.pyplot as plt
+
 def selectionSort(arr):
-	for i in range(len(arr)-1):
-		pos = i
-		for j in range(i+1, len(arr)):
-			if arr[j] < arr[i]:
-				pos = j
-		arr[i], arr[pos] = arr[pos], arr[i]
-    
+    for i in range(len(arr)-1):
+        pos = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[i]:
+                pos = j
+        arr[i], arr[pos] = arr[pos], arr[i]
+
 current_n = 10
 times = []
-for _ in range(1500):
-	arr = [random.randint(1, current_n) for i in range(current_n)]
-	start_time = tm.time()
-	selectionSort(arr)
-	end_time = tm.time()	
-	times.append((current_n, (end_time-start_time)))
-	current_n += 70
-print(times)
+for _ in range(30):
+    arr = [random.randint(1, current_n) for i in range(current_n)]
+    start_time = tm.time()
+    selectionSort(arr)
+    end_time = tm.time()
+    times.append((current_n, (end_time-start_time)))
+    current_n += 100
+xplot = [times[i][0] for i in range(len(times))]
+yplot = [times[i][1] for i in range(len(times))]
+plt.scatter(xplot, yplot, marker="+")
+plt.show()
