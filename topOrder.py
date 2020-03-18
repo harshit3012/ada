bbee9361ee8ccf486a5cf02db0cad9d9e6c3a825
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-03-19 02:15:50 +0530

    inc

--- topOrder.py
+++ topOrder.py
@@ -1,27 +1,28 @@
+#This program does topological sorting
 edges = int(input("enter number of edges: "))
 n_vertices = int(input("enter number of vertices: "))
-neighbours = {}
+neighbours_dict = {}
 print("enter edges [start] [end]: ")
 for i in range(edges):
     start, end = [int(x) for x in input().split()]
-    if not neighbours.get(start):
-        neighbours[start] = [end]
+    if not neighbours_dict.get(start):
+        neighbours_dict[start] = [end]
     else:
-        neighbours[start].append(end)
+        neighbours_dict[start].append(end)
     
 def topOrder(node, visited, stack):
     visited[node] = True
 
-    if neighbours.get(node):
-        for n in neighbours[node]:
+    if neighbours_dict.get(node):
+        for n in neighbours_dict[node]:
             if not visited[n]:
                 topOrder(n, visited, stack)
     
     stack.append(node)
 
-visited = [False] * 6 
-stack = []
-for vertex in neighbours.keys():
+visited = [False] * n_vertices
+main_stack = []
+for vertex in neighbours_dict.keys():
     if not visited[vertex]:
-        topOrder(vertex, visited, stack)
-print(stack[::-1])
+        topOrder(vertex, visited, main_stack)
+print(main_stack[::-1])
