8f6efc23166e78932944cb5b360980be380b11c7
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-03-19 02:20:27 +0530

    inc

--- topSort_lexico.py
+++ topSort_lexico.py
@@ -1,16 +1,16 @@
 edges = int(input("enter number of edges: "))
 n_vertices = int(input("enter number of vertices: "))
-neighbours = {}
+neighbours_dict = {}
 print("enter edges [start] [end]: ")
 for i in range(edges):
     start, end = [int(x) for x in input().split()]
-    if not neighbours.get(start):
-        neighbours[start] = [end]
+    if not neighbours_dict.get(start):
+        neighbours_dict[start] = [end]
     else:
-        neighbours[start].append(end)
+        neighbours_dict[start].append(end)
     
 indegrees = [0] * n_vertices 
-for vertices in neighbours.values():
+for vertices in neighbours_dict.values():
     for vertex in vertices:
         indegrees[vertex] += 1
     
@@ -21,8 +21,8 @@ while len(noIndegreeVertices) > 0:
     topOrder.append(min_noIndegreeVertex)
     noIndegreeVertices.discard(min_noIndegreeVertex)
     
-    if neighbours.get(min_noIndegreeVertex):
-        for neighbour in neighbours[min_noIndegreeVertex]:
+    if neighbours_dict.get(min_noIndegreeVertex):
+        for neighbour in neighbours_dict[min_noIndegreeVertex]:
             indegrees[neighbour] -= 1
             if indegrees[neighbour] == 0:
                 noIndegreeVertices.add(neighbour) 
