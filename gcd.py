c0015d0fc71591300eea1197fde315469e9d621c
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-05-12 13:12:11 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,10 +1,10 @@
-def hcf(a, b):
+def gcd1(a, b):
     if(a < b):
         (a, b) = (b, a)
     if(a%b == 0):
         return b
     else:
-        return hcf(b, a%b)
+        return gcd1(b, a%b)
 n = int(input("n : "))
 m = int(input("m : "))
-print(str(hcf(n, m)))
+print(str(gcd1(n, m)))
