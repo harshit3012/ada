25b51da6eac44273ef36107e214df055ab68fbcf
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-05-12 14:06:21 +0530

    Update stringMatch.py

--- stringMatch.py
+++ stringMatch.py
@@ -8,9 +8,16 @@ def string_match(text, pattern):
             return i
     return -1
 
-print(string_match("Cool string matching algo", "string"))
-print(string_match("computer science engineering", "science"))
-print(string_match("working is fun", "work"))
-print(string_match("cabaaabc", "abc"))
-print(string_match("bccdabc", "bc"))
-print(string_match("aaaaabc", "bcd"))
+# print(string_match("Cool string matching algo", "string"))
+# print(string_match("computer science engineering", "science"))
+# print(string_match("working is fun", "work"))
+# print(string_match("cabaaabc", "abc"))
+# print(string_match("bccdabc", "bc"))
+# print(string_match("aaaaabc", "bcd"))
+text = print('Enter the text :', end="")
+pattern = print('Enter the pattern :', end="")
+pos = string_match(text, pattern)
+if(pos == -1):
+    print("Could not find the pattern in the text!")
+else:
+    print(f'Pattern found in the text at {pos}th position')
