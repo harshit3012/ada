32888f461c1a11ab549bc665fdbf6a8e6d5aaaee
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-05-12 13:49:31 +0530

    Update dfs_connected.py

--- dfs_connected.py
+++ dfs_connected.py
@@ -1,11 +1,30 @@
-import string
-n = int(input("Enter the number of nodes : "))
-out_graphs1 = {}
-for i in list(string.ascii_lowercase)[0:n]:
-    print("Enter the nodes directed by node "+i, end=": ")
-    out_graphs1[i] = [j for j in input().split()]
-print(out_graphs1.keys())
-visited = {}
-for i in list(out_graphs1.keys()):
-    
-#inc
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+n = int(input("Enter number of vertices: "))
+e = int(input("Enter the number of edges: "))
+if(n == 0 or e == 0):
+    print('Order-zero graph!')
+    exit()
+graph = {}
+print("Enter the edges in the format [start] [end]")
+for i in range(e):
+    start, end = list(input("").split())
+    if not graph.get(start):
+        graph[start] = set([end])
+    else:
+        graph[start].add(end)
+    if not graph.get(end):
+        graph[end] = set([start])
+    else:
+        graph[end].add(start)
+
+if len(dfs(graph, list(graph.keys())[0])) == n:
+    print("Connected")
+else:
+    print("Not connected")
